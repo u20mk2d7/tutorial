af03379ed7a1518708a0c1eeed439dff59fce4e9
u20mk2d7 <u20mk2d7@gmail.com>
2026-04-27 09:51:16 +0700

    update

--- src/asio/timer1.cpp
+++ src/asio/timer1.cpp
@@ -1,1 +1,16 @@
+#include <boost/asio.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl.hpp>
+#include <iostream>
+#include <string>
 
+void tuttimer() {
+  boost::asio::io_context io;
+  boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
+
+  t.wait();
+
+  std::cout << "Hello, world!" << std::endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,38 @@
 #include <boost/asio.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl.hpp>
 #include <iostream>
+#include <string>
+
+void f() {
+  try {
+    // 1. Hire the Foreman
+    boost::asio::io_context ioc;
+
+    // 2. Buy the empty Copper Wire (Hand it to the foreman)
+    boost::asio::ip::tcp::socket mycable{ioc};
+
+    // 3. Hire the Navigator (Resolver) to find the doors
+    boost::asio::ip::tcp::resolver navigator{ioc};
+    boost::asio::ip::tcp::resolver::results_type list_of_doors =
+        navigator.resolve("stream.binance.com", "443");
+    std::cout << "Found the doors! Handing cable to the Smart Plumber...\n";
+
+    // 4. THE SMART PLUMBER (asio::connect)
+    // We pass the empty cable, and the list of doors.
+    // It does the 3-Way Handshake for us!
+    boost::asio::connect(mycable, list_of_doors);
+
+    std::cout << "SUCCESS! The physical TCP highway to Binance is open!\n";
+
+  } catch (std::exception const& e) {
+    std::cerr << "The Plumber failed: " << e.what() << "\n";
+  }
+}
+
 int main() {
-  boost::asio::io_context io;
-  boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
-
-  t.wait();
-
-  std::cout << "Hello, world!" << std::endl;
-
+  f();
   return 0;
 }
