23b5f2d9d7762a83829a28cfa275a399962f7190
u20mk2d7 <u20mk2d7@gmail.com>
2026-04-27 10:39:51 +0700

    update

--- unix.sh
+++ unix.sh
@@ -2,7 +2,8 @@ git add .
 git commit -m "update"
 git push origin main
 
-rm -rf build/
+#rm -rf build/
 cmake -S . -B build
 cmake --build build
-./build/cornerstone
+PROJECT_NAME=$(grep -iE "^ *project" CMakeLists.txt | cut -d '(' -f2 | awk '{print $1}' | tr -d ')')
+./build/"$PROJECT_NAME"
